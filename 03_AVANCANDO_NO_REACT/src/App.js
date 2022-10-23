c6d358bd8a2f3bb18fcfea499388cb52eb0cc11f
Bruno <bruno1_oliveira@hotmail.com>
2022-10-23 10:01:05 -0300

    Seção 03 React - Props, destructuring props, reaproveitamento de componentes, renderização de lista em componentes

--- 03_AVANCANDO_NO_REACT/src/App.js
+++ 03_AVANCANDO_NO_REACT/src/App.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import City from "./assets/city.jpg";
+import CarDetails from './components/CarDetails';
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+  const name = "Bianca";
+  const [userName] = useState("Bruno");
+
+  const cars = [
+    {id: 1, brand: "Ferrari", color: "Amarelo", newCar: true, km: 0},
+    {id: 2, brand: "KIA", color: "Branco", newCar: false, km: 15200},
+    {id: 3, brand: "Renault", color: "Azul", newCar: false, km: 5890},
+  ];
+
   return (
     <div className="App">
         <h1>Seção 03 - Avançando no React</h1>
@@ -14,13 +26,31 @@ function App() {
         </div>
         {/* Imagem em asset */}
         <div>
-          <img src={City} alt="City"/>
+          <img src={City} alt="City" />
         </div>
-        <ManageData/>
-        <ListRender/>
-        <ConditionalRender/>
+        <ManageData />
+        <ListRender />
+        <ConditionalRender />
+        {/* props */}
+        <ShowUserName name={userName} />
+        <ShowUserName name={name} />
+        <ShowUserName name="Bruno e Bianca" />
+        {/* destructuring */}
+        <CarDetails brand="VW" km={100000} color="Azul" newCar={false} />
+        {/* reaproveitamento */}
+        <CarDetails brand="Ford" km={0} color="Vermelho" newCar={true} />
+        <CarDetails brand="Fiat" km={4500} color="Branco" newCar={false} />
+        {/* loop em array de objetos */}
+        {cars.map((car) => (
+          <CarDetails 
+          brand={car.brand} 
+          color={car.color} 
+          km={car.km} 
+          newCar={car.newCar} 
+          />
+        ))};
     </div>
   );
-}
+};
 
 export default App;
---
+++ 03_AVANCANDO_NO_REACT/src/components/CarDetails.js
@@ -0,0 +1,15 @@
+const CarDetails = ({brand, km, color, newCar}) => {
+  return (
+    <div>
+        <h2>Detalhes do carro</h2>
+        <ul>
+            <li>Marca: {brand}</li>
+            <li>KM: {km}</li>
+            <li>Cor: {color}</li>
+        </ul>
+        {newCar && <p>Esse carro é novo!</p>}
+    </div>
+  );
+};
+
+export default CarDetails;
