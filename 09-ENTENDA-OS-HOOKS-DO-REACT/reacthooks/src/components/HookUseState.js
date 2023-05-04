db7a4f5fc1a5f5f4f7b7480016f32c8d81124dd7
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2023-05-03 23:28:45 -0300

    Feat: atualizando aula hooks - useState

--- 09-ENTENDA-OS-HOOKS-DO-REACT/reacthooks/src/components/HookUseState.js
+++ 09-ENTENDA-OS-HOOKS-DO-REACT/reacthooks/src/components/HookUseState.js
@@ -1,21 +1,48 @@
 import React, { useState } from 'react'
 
 const HookUseState = () => {
+    // 1 - useState
     let userName = 'Bianca'
     const [ name, setName ] = useState('Bruno');
 
     const handleChangeName = () => {
         userName = 'Bianca Cristine';
         setName('Bruno Oliveira');
+
+        console.log(userName)
+    }
+    console.log(name)
+
+    // 2 - useState e input
+    const [ age, setAge ] = useState(18)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // envio a uma API
+        console.log(age)
     }
 
     return (
         <div>
+            {/* 1 - useState */}
             <h2>useState</h2>
             <p>Variável: {userName}</p>
             <p>useState: {name}</p>
             <button onClick={handleChangeName}>Mudar nomes</button>
 
+            {/* 2 - useState e input */}
+            <p>Digite a sua idade</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                 type="text" 
+                 value={age} 
+                 onChange={(e) => setAge(e.target.value)}
+                />
+                <input type="submit" value="Enviar" />
+            </form>
+
+            <p>Você tem {age} anos.</p>
             <hr />
 
         </div>
